fa71bc31a31d55294234933a622732ace43b5b50
neo050 <neoray.asking666@gmail.com>
2025-02-04 13:02:28 +0200

    new

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -4,12 +4,14 @@ for (let i =0 ;i< arr.length;i+=1)
     arr[i].addEventListener("click",function ()
     {
         makesound(this.innerHTML)
+        buttonAnimation(this.innerHTML);
     });
 }
 
 
 document.addEventListener("keypress",function(event){
      makesound(event.key);
+     buttonAnimation(event.key);
 });
 
 function makesound(key)
@@ -37,7 +39,7 @@ function makesound(key)
             break
         case"k":
             var tom3= new Audio("sounds/tom-3.mp3");
-            tom3.play();
+            tom3.play(); 
             break
          case"l":
             var tom4= new Audio("sounds/tom-4.mp3");
@@ -45,4 +47,14 @@ function makesound(key)
             break
         default: console.log(keypress_event_key);
     }
+}
+
+function buttonAnimation(key)
+{
+
+    var  actuveButton =document.querySelector("."+key);
+    actuveButton.classList.add("pressed");
+
+    setTimeout(function(){actuveButton.classList.remove("pressed");
+    } ,100)
 }
